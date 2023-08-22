0d877e51fcabb47366ea1962daf685200bf71076
Terence Hughes <Terencehughes450@gmail.com>
2023-08-22 16:41:01 -0600

    finished lab

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,16 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  // create a const that is our .map of each individual project, we create a projectitem for each or project
+  const eachObj = projects.map((project)=>(
+    <ProjectItem key={project.id} name={project.name} about ={project.about} technologies={project.technologies}/>
+  ))
+
+  console.log(eachObj)
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{eachObj}</div>
     </div>
   );
 }
